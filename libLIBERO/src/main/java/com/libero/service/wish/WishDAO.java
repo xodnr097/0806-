c88850d582382e099b4a4bc466a93ec4e101adf8
xodnr <xodnr@DESKTOP-J96CDO2>
2020-07-21 18:07:05 +0900

    태욱 : 좋아요 작업중/ 버그 수정

--- libLIBERO/src/main/java/com/libero/service/wish/WishDAO.java
+++ libLIBERO/src/main/java/com/libero/service/wish/WishDAO.java
@@ -1,8 +1,19 @@
 package com.libero.service.wish;
 
+import java.util.HashMap;
+import java.util.List;
+
+import com.libero.service.domain.Product;
 
 public interface WishDAO {
 	
-	public int getWish(int prodNo, String userId);
+	public List<Product> getWishList(String userId);
+
+	public void addWish(HashMap<String, Object> hashMap);
+
+	public void removeWish(HashMap<String, Object> hashMap);
+
+
+
 	
 }
--- libLIBERO/src/main/java/com/libero/service/wish/impl/WishDAOImpl.java
+++ libLIBERO/src/main/java/com/libero/service/wish/impl/WishDAOImpl.java
@@ -1,13 +1,17 @@
 package com.libero.service.wish.impl;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
+import com.libero.service.domain.Product;
 import com.libero.service.wish.WishDAO;
 
-@Repository("WishDAOImpl")
+@Repository("wishDAOImpl")
 public class WishDAOImpl implements WishDAO {
 	
 	@Autowired
@@ -17,9 +21,19 @@ public class WishDAOImpl implements WishDAO {
 		this.sqlSession = sqlSession;
 	}
 	@Override
-	public int getWish(int prodNo, String userId) {
-		// TODO Auto-generated method stub
-		return sqlSession.selectOne("ProductMapper.getWish");
+	public List<Product> getWishList(String userId) {
+		
+			return sqlSession.selectList("ProductMapper.getWishProduct", userId);
+	}
+	
+	@Override
+	public void addWish(HashMap<String, Object> hashMap) {
+			sqlSession.insert("ProductMapper.addWish", hashMap);
+	}
+	
+	@Override
+	public void removeWish(HashMap<String, Object> hashMap) {
+			sqlSession.delete("ProductMapper.removeWish", hashMap);
 	}
 	
 }
